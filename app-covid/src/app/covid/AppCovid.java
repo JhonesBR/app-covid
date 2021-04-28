b8c263bb60413232cbb4d8beef84ad77e589acae
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-04-28 16:24:52 -0300

    Initial

---
+++ app-covid/src/app/covid/AppCovid.java
@@ -0,0 +1,12 @@
+package app.covid;
+
+/**
+ * @author João Vitor Oliveira de Melo
+ */
+public class AppCovid {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
